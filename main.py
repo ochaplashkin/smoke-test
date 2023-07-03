2a918b64a414033c63f919e761218d95a20e9cc6
Oleg Chaplashkin <ochaplashkin@tarantool.org>
2023-07-03 19:44:17 +0400

    4

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return HTMLResponse(content='<h1>Hello world</h1>')
+
+@app.get("/my-branch")
+async def root():
+    return HTMLResponse(content='<h1>Hello my-branch!</h1>')
